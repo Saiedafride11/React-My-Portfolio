daf7fa538717321c98225a9cd05f5cbae4aba982
Coders Key <designer.afride@gmail.com>
2021-11-25 03:59:05 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://ps.w.org/under-construction-page/assets/screenshot-2.png?rev=1840052" width="100%" alt="" />
+    </div>
+  );
+}
+
+export default App;
